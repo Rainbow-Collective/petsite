546d807b0f0bd9c81ba831e4a531137c62b93e08
Ashton MacKenzie <pikeminnow@gmail.com>
2022-11-29 21:20:58 -0500

    before git stash pop

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,13 @@ class UsersController < ApplicationController
 
   # POST /users
   def create
-    binding.break
-    render json: User.create!(user_params),  status: 201
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
   end
 
   # PATCH/PUT /users/1
@@ -41,6 +46,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :password_confirmation, :sprite_id, :password, :password_digest)
+      params.require(:user).permit(:name, :password_digest)
     end
 end
--- db/migrate/20221130022026_create_users.rb
+++ db/migrate/20221130022026_create_users.rb
@@ -3,7 +3,6 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :name
       t.string :password_digest
-      t.references :sprite, null: false, foreign_key: true
 
       t.timestamps
     end
