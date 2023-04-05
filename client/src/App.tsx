dc7f0187bf7c767cef6f51633ba4136dfeb4f152
circuitsunfish <802-829-1624@ashtonmackenzie.com>
2023-04-05 19:04:15 -0400

    removed generated app items

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="nav-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="nav-link"
-          href="https://ashtonmackenzie.com/portfolio/the-guild-of-the-magi-everything/"
-        >
-          History
-        </a>
+      <header className="app-body">
+        <div id="external-links">
+          <a
+            className="nav-link"
+            href="https://ashtonmackenzie.com/portfolio/the-guild-of-the-magi-everything/"
+          >
+            History
+          </a>
+        </div>
       </header>
     </div>
   );
