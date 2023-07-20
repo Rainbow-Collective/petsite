d33bdd0173f85394a00cc2e3dcf8dc96b63b4cac
circuitsunfish <802-829-1624@ashtonmackenzie.com>
2023-07-20 14:09:39 -0400

    clearing empty props array for now since there is no state to track at this time

--- client/src/navigation/NavHeader.tsx
+++ client/src/navigation/NavHeader.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
-type Props = {}
+// type Props = {}
 
-function NavHeader({}: Props) {
-  return (
-    <header className="navigation">
-    <h1>TGOTM</h1>
-    <h2>The Guild of the Magi</h2>
-  </header>
-  )
+// function NavHeader({}: Props) {
+function NavHeader() {
+    return (
+        <header className="navigation">
+            <h1>TGOTM</h1>
+            <h2>The Guild of the Magi</h2>
+        </header>
+    )
 }
 
 export default NavHeader
